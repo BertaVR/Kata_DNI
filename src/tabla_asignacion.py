60df641505ff9db2a5225a923045e0b23c5e3b6a
Berta Vergés <72273706+BertaVR@users.noreply.github.com>
2021-01-21 11:41:41 +0100

    feat: :art: crear estructura de carpetas

---
+++ src/tabla_asignacion.py
@@ -0,0 +1,2 @@
+class Tabla_asignacion:
+    tabla_letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E']
---
+++ test/test_validar_dni.py
@@ -0,0 +1,6 @@
+from src.validar_dni import *
+
+def test_validar_formato():
+    assert Validar_DNI('41569305').validar_formato == True
+    assert Validar_DNI('4155965').validar_formato == False, 'es demasiado corto'
+
